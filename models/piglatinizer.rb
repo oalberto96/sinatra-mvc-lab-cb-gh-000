6ce78fedcd963d4a00a60cdfa2e80bdbc2ed654b
Alberto Ochoa <ochoapublic@gmail.com>
2018-07-19 19:38:54 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,7 +1,13 @@
 class PigLatinizer
-  
-  def piglatinaze(text) 
-    @text
+
+  def is_vowel?(letter)
+    letter =~ /[AEIOUaeiou]/
+  end
+
+  def piglatinaze(text)
+  end
+
+  def translate_word(word)
+    
   end
 end
-  
